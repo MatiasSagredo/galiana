baa5de52da8f12f2faa07e8636a24ff219e45803
MatiasSagredo <ma.sagredom@duocuc.cl>
2025-05-14 11:17:30 -0400

    init

---
+++ src/test/java/com/galiana_project/cl/galiana/GalianaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galiana_project.cl.galiana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalianaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
