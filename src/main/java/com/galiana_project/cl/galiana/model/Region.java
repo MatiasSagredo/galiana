422919a4696939f91ff41a342a105091c7f6aaee
MatiasSagredo <ma.sagredom@duocuc.cl>
2025-05-15 12:11:54 -0400

    agregar comuna y region, y modificar application.properties

---
+++ src/main/java/com/galiana_project/cl/galiana/model/Region.java
@@ -0,0 +1,24 @@
+package com.galiana_project.cl.galiana.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "region")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Region {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "nombre", nullable = false,length = 50)
+    private String nombre;
+}
